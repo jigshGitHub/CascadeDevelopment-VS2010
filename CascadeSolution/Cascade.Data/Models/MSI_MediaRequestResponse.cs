d4a92f62ca993239399ebb7dd1e2173830a7ec39
Jignesh Shah <jigsh@hotmail.com>
2013-02-18 11:58:46 -0500

    MediaRequest-PArt4

--- CascadeSolution/Cascade.Data/Models/MSI_MediaRequestResponse.cs
+++ CascadeSolution/Cascade.Data/Models/MSI_MediaRequestResponse.cs
@@ -19,16 +19,16 @@ namespace Cascade.Data.Models
             this.MSI_MediaRequestedTypes = new HashSet<MSI_MediaRequestedTypes>();
         }
     
-        public System.Guid Id { get; set; }
+        public string Id { get; set; }
         public string AgencyId { get; set; }
-        public string Account { get; set; }
+        public string ACCOUNT { get; set; }
         public string OriginalAccount { get; set; }
         public string Portfolio { get; set; }
-        public string Lender { get; set; }
+        public string Originator { get; set; }
         public string SSN { get; set; }
-        public string AccountName { get; set; }
+        public string NAME { get; set; }
         public Nullable<System.DateTime> OpenDate { get; set; }
-        public Nullable<System.DateTime> CODate { get; set; }
+        public Nullable<System.DateTime> ChargeOffDate { get; set; }
         public string Seller { get; set; }
         public System.DateTime RequestedDate { get; set; }
         public Nullable<System.DateTime> RespondedDate { get; set; }
